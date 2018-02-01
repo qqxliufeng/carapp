6720e08bddff6b2876b29b254787f686d295d6be
qqxliufeng <596484713@qq.com>
2018-02-01 19:18:20 +0800

    论谈页面

--- src/main/java/com/android/ql/lf/carapp/ui/fragments/bottom/MainCommunityFragment.kt
+++ src/main/java/com/android/ql/lf/carapp/ui/fragments/bottom/MainCommunityFragment.kt
@@ -1,22 +1,108 @@
 package com.android.ql.lf.carapp.ui.fragments.bottom
 
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
-import com.android.ql.lf.carapp.ui.fragments.BaseFragment
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.android.ql.lf.carapp.R
+import com.android.ql.lf.carapp.ui.activities.MainActivity
+import com.android.ql.lf.carapp.ui.adapter.OrderListForMineForWaitingWorkAdapter
+import com.android.ql.lf.carapp.ui.fragments.BaseRecyclerViewFragment
+import com.bumptech.glide.Glide
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.sunfusheng.marqueeview.MarqueeView
+import com.youth.banner.BannerConfig
+import com.youth.banner.loader.ImageLoader
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+import kotlinx.android.synthetic.main.fragment_main_community_layout.*
 
 /**
  * Created by lf on 18.1.24.
  * @author lf on 18.1.24
  */
-class MainCommunityFragment : BaseFragment(){
+class MainCommunityFragment : BaseRecyclerViewFragment<String>() {
+
 
     companion object {
-        fun newInstance():MainCommunityFragment{
+        fun newInstance(): MainCommunityFragment {
             return MainCommunityFragment()
         }
     }
 
-    override fun getLayoutId() = android.R.layout.simple_list_item_1
+    private val topList = arrayListOf<Int>()
+    private lateinit var topMarqueeView:MarqueeView
+
+    override fun getLayoutId() = R.layout.fragment_main_community_layout
 
     override fun initView(view: View?) {
+        super.initView(view)
+        val height = (mContext as MainActivity).statusHeight
+        val param = mRlCommunityTitleContainer.layoutParams as ViewGroup.MarginLayoutParams
+        param.topMargin = height
+        mRlCommunityTitleContainer.layoutParams = param
+        mBannerMainCommunity.setImageLoader(object : ImageLoader() {
+            override fun displayImage(context: Context?, path: Any?, imageView: ImageView?) {
+                Glide.with(context).load(path as Int).into(imageView)
+            }
+        })
+        mBannerMainCommunity
+                .setImages(arrayListOf(R.drawable.test_pic01, R.drawable.test_pic01, R.drawable.test_pic01, R.drawable.test_pic01))
+                .setDelayTime(3000)
+                .setBannerStyle(BannerConfig.CIRCLE_INDICATOR)
+                .start()
+        (0 until 10).forEach {
+            topList.add(R.drawable.test_pic01)
+        }
+        val topView = View.inflate(mContext, R.layout.layout_main_community_top_layout, null)
+        val topRecyclerView = topView.findViewById<RecyclerView>(R.id.mRvMainCommunityTopContainer)
+        topMarqueeView = topView.findViewById<MarqueeView>(R.id.mMvMainCommunityTopContainer)
+        topRecyclerView.layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false)
+        topRecyclerView.adapter = TopRecyclerViewAdapter(R.layout.layout_main_community_top_item_layout, topList)
+
+        val info = ArrayList<String>()
+        info.add("1. 大家好，我是孙福生。")
+        info.add("2. 欢迎大家关注我哦！")
+        info.add("3. GitHub帐号：sfsheng0322")
+        info.add("4. 新浪微博：孙福生微博")
+        info.add("5. 个人博客：sunfusheng.com")
+        info.add("6. 微信公众号：孙福生")
+        topMarqueeView.startWithList(info)
+
+        mBaseAdapter.addHeaderView(topView)
+    }
+
+    override fun createAdapter(): BaseQuickAdapter<String, BaseViewHolder> =
+            OrderListForMineForWaitingWorkAdapter(R.layout.adapter_order_list_for_mine_for_having_calculate_item_layout, mArrayList)
+
+    override fun onRefresh() {
+        super.onRefresh()
+        testAdd("")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mBannerMainCommunity.startAutoPlay()
+        topMarqueeView.startFlipping()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mBannerMainCommunity.stopAutoPlay()
+        topMarqueeView.stopFlipping()
+    }
+
+    class TopRecyclerViewAdapter(layoutId: Int, list: ArrayList<Int>) : BaseQuickAdapter<Int, BaseViewHolder>(layoutId, list) {
+        override fun convert(helper: BaseViewHolder?, item: Int?) {
+            val imageView = helper!!.getView<ImageView>(R.id.mIvMainCommunityTopItemImage)
+            Glide.with(mContext)
+                    .load(item)
+                    .bitmapTransform(RoundedCornersTransformation(mContext, 8, 0))
+                    .into(imageView)
+            imageView.setColorFilter(Color.parseColor("#77000000"))
+        }
     }
 }
--- src/main/java/com/android/ql/lf/carapp/ui/fragments/order/OrderImageUpLoadFragment.kt
+++ src/main/java/com/android/ql/lf/carapp/ui/fragments/order/OrderImageUpLoadFragment.kt
@@ -4,27 +4,23 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.net.Uri
-import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import com.android.ql.lf.carapp.R
 import com.android.ql.lf.carapp.ui.adapter.OrderImageUpLoadAdapter
 import com.android.ql.lf.carapp.ui.fragments.BaseNetWorkingFragment
-import com.android.ql.lf.carapp.ui.fragments.BaseRecyclerViewFragment
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
+import com.chad.library.adapter.base.listener.OnItemClickListener
 import com.zhihu.matisse.Matisse
 import com.zhihu.matisse.MimeType
 import com.zhihu.matisse.engine.impl.GlideEngine
 import com.zhihu.matisse.internal.entity.CaptureStrategy
 import kotlinx.android.synthetic.main.fragment_order_image_upload_layout.*
+import org.jetbrains.anko.support.v4.toast
 
 /**
  * Created by lf on 18.1.29.
@@ -33,7 +29,7 @@ import kotlinx.android.synthetic.main.fragment_order_image_upload_layout.*
 class OrderImageUpLoadFragment : BaseNetWorkingFragment() {
 
     private val mArrayList = arrayListOf<ImageBean>()
-    private val mBaseAdapter : BaseQuickAdapter<ImageBean,BaseViewHolder> by lazy {
+    private val mBaseAdapter: BaseQuickAdapter<ImageBean, BaseViewHolder> by lazy {
         OrderImageUpLoadAdapter(R.layout.adapter_order_image_up_load_item_layout, mArrayList)
     }
 
@@ -45,21 +41,32 @@ class OrderImageUpLoadFragment : BaseNetWorkingFragment() {
     override fun getLayoutId() = R.layout.fragment_order_image_upload_layout
 
     override fun initView(view: View?) {
-        test.setOnClickListener {
-            Matisse.from(this)
-                    .choose(MimeType.allOf())
-                    .imageEngine(GlideEngine())
-                    .capture(true)
-                    .maxSelectable(3)
-                    .captureStrategy(CaptureStrategy(true, "com.android.ql.lf.carapp.fileProvider"))
-                    .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
-                    .thumbnailScale(0.85f)
-                    .theme(R.style.my_matisse_style)
-                    .forResult(0)
-        }
-        mArrayList.add(ImageBean(R.drawable.img_icon_add_photo,null))
+        mArrayList.add(ImageBean(R.drawable.img_icon_add_photo, null))
         mRecyclerView.adapter = mBaseAdapter
         mRecyclerView.layoutManager = GridLayoutManager(mContext, 2)
+        mRecyclerView.addOnItemTouchListener(object : OnItemClickListener() {
+            override fun onSimpleItemClick(adapter: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+                if (mBaseAdapter.itemCount - 1 >= 3) {
+                    toast("最多只能选择三张图片")
+                    return
+                }
+                val item = mBaseAdapter.getItem(position)
+                if (item != null) {
+                    if (item.resId != null) {
+                        Matisse.from(this@OrderImageUpLoadFragment)
+                                .choose(MimeType.ofAll())
+                                .imageEngine(GlideEngine())
+                                .capture(true)
+                                .maxSelectable(3 - (mBaseAdapter.itemCount - 1))
+                                .captureStrategy(CaptureStrategy(true, "com.android.ql.lf.carapp.fileProvider"))
+                                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                                .thumbnailScale(0.85f)
+                                .theme(R.style.my_matisse_style)
+                                .forResult(0)
+                    }
+                }
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
@@ -71,12 +78,12 @@ class OrderImageUpLoadFragment : BaseNetWorkingFragment() {
         if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
             val uris = Matisse.obtainResult(data)
             uris.forEach {
-                mArrayList.add(0,ImageBean(null,it))
+                mArrayList.add(0, ImageBean(null, it))
             }
             mBaseAdapter.notifyDataSetChanged()
         }
     }
 
-    class ImageBean(var resId:Int?,var uriPath:Uri?)
+    class ImageBean(var resId: Int?, var uriPath: Uri?)
 
 }
